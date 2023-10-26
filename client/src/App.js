6787e8ffa80680aa605a56f3d4d29118f08e2d70
saloni kumawat <545saloni@gmail.com>
2023-10-26 13:37:36 +0200

    Add static data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+const questions = [
+  { id: 1, question: "What is the capital of France?", answer: "Paris" },
+  { id: 2, question: "Who wrote 'Romeo and Juliet'?", answer: "William Shakespeare" },
+  { id: 3, question: "What is the largest planet in our solar system?", answer: "Jupiter" },
+  { id: 4, question: "In which year did World War II end?", answer: "1945" },
+  { id: 5, question: "What is the main ingredient in guacamole?", answer: "Avocado" },
+  { id: 6, question: "Who developed the theory of relativity?", answer: "Albert Einstein" }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>Hello world</h1>
     </div>
   );
 }
